c7fe70434c90c2db144a34a3ab13729a42f723ec
AmjadRabby <glamarrabby@gmail.com>
2020-09-29 01:09:30 +0600

    node mongo start

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,22 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
+//     authDomain: "burj-al-arab.firebaseapp.com",
+//     databaseURL: "https://burj-al-arab.firebaseio.com",
+//     projectId: "burj-al-arab",
+//     storageBucket: "burj-al-arab.appspot.com",
+//     messagingSenderId: "244522202387",
+//     appId: "1:244522202387:web:f3801d0588387140e1d6e6"
+//   };
+ 
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBEtA6-K03UMnVjA16iBU3c1k7VGvGI09w",
+    authDomain: "burj-al-arab-46402.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-46402.firebaseio.com",
+    projectId: "burj-al-arab-46402",
+    storageBucket: "burj-al-arab-46402.appspot.com",
+    messagingSenderId: "692996273136",
+    appId: "1:692996273136:web:b344c587cd36a0c907a335"
+  };
+
+  export default firebaseConfig;
