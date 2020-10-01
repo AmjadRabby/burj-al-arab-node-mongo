653d2d3c0c053800dd54c641762936fe8b4b77b7
AmjadRabby <glamarrabby@gmail.com>
2020-10-01 15:30:36 +0600

    mongodb jwt done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -21,15 +21,25 @@ const Login = () => {
             const {displayName, email} = result.user;
             const signedInUser = {name: displayName, email} 
             setLoggedInUser(signedInUser);
-            history.replace(from);
-            // ...
+            storeAurthToken()
+            
           }).catch(function(error) {
             const errorMessage = error.message;
             console.log(errorMessage);
           });
     }
+
+    const storeAurthToken = () => {
+        firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
+        .then(function(idToken) {
+            sessionStorage.setItem('token', idToken);
+            history.replace(from);
+          }).catch(function(error) {
+            console.log(error);
+          });
+    }
     return (
-        <div>
+        <div style={{textAlign: 'center'}}>
             <h1>This is Login</h1>
             <button onClick={handleGoogleSignIn}>Google Sign in</button>
         </div>
